d521093f2e6145da3df3ddd2e89331eaaf1fff21
ttolofari <tolofarit@gmail.com>
2024-06-25 19:27:11 -0700

    print all records

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98: This prints all numbers
+ * from n to 98.
+ * @n: This is the input
+ */
+void print_to_98(int n) 
+{
+    int i;
+    
+    if (n <= 98) 
+    {
+        for (i = n; i <= 98; i++) 
+        {
+            if (i != n) 
+            {
+                printf(", ");
+            }
+            printf("%d", i);
+        }
+    } 
+    else 
+    {
+        for (i = n; i >= 98; i--) 
+        {
+            if (i != n) 
+            {
+                printf(", ");
+            }
+            printf("%d", i);
+        }
+    }
+    printf("\n");
+}
