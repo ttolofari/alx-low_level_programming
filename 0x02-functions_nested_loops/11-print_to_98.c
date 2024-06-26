9355b2b8f56d30a7018913945b654d4df7d0ac75
ttolofari <tolofarit@gmail.com>
2024-06-25 19:32:27 -0700

    print all records

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,29 @@
  */
 void print_to_98(int n) 
 {
-    int i;
+	int i;
     
-    if (n <= 98) 
-    {
-        for (i = n; i <= 98; i++) 
-        {
-            if (i != n) 
-            {
-                printf(", ");
-            }
-            printf("%d", i);
-        }
-    } 
-    else 
-    {
-        for (i = n; i >= 98; i--) 
-        {
-            if (i != n) 
-            {
-                printf(", ");
-            }
-            printf("%d", i);
-        }
-    }
-    printf("\n");
+    	if (n <= 98) 
+	{
+		for (i = n; i <= 98; i++) 
+		{
+			if (i != n) 
+			{
+				printf(", ");
+			}
+			printf("%d", i);
+		}
+	} 
+    	else 
+	{
+        	for (i = n; i >= 98; i--) 
+        	{
+            		if (i != n) 
+            		{
+                		printf(", ");
+           	 	}
+            		printf("%d", i);
+       		 }
+   	 }
+   	 printf("\n");
 }
