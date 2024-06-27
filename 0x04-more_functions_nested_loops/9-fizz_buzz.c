89751a5469a9e24e4318569d3b25f8dd1138be05
ttolofari <tolofarit@gmail.com>
2024-06-27 16:23:12 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,24 @@ int main(void)
 {
 	int i;
 
-	for (int i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
+	for (i = 1; i < 100; i++)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
-		} else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		} else if (i % 5 == 0)
+		}
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-		} else
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
 		{
 			printf("%d ", i);
 		}
-	}
-	
+	printf("Buzz");
 	printf("\n");
-	
 	return (0);
 }
