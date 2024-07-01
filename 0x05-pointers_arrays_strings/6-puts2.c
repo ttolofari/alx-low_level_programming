4913f93a42e03630754b91fa54ae81da86c02e95
ttolofari <tolofarit@gmail.com>
2024-07-01 15:04:35 -0700

    betty styled

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 -  function that prints every other character of a string
  * starting with the first character, followed by a new line
- * @s : input string
+ * @str : input string
  */
 
 void puts2(char *str)
@@ -12,7 +12,8 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0) {
+		if (i % 2 == 0)
+		{
 			_putchar(str[i]);
 		}
 	}
