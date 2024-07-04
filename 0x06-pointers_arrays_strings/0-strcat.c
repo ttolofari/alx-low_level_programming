bfc61e1c60a8dd8ff6b90fc33d6d8d8c650cc9eb
ttolofari <tolofarit@gmail.com>
2024-07-04 15:39:54 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcat - function to concatenate two strings
+ * @*dest :  destination
+ * @*src :  source
+ *
+ * Return : pointer to the resultant string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
