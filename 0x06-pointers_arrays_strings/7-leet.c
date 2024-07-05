567a520b70800dae941681179fa973354bb32baa
ttolofari <tolofarit@gmail.com>
2024-07-04 18:08:21 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ * @str : input string
+ *
+ * Return : output string;
+ */
+char *leet(char *str)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+
+	return (str);
+}
