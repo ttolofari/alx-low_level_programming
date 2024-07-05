be4f4995610e21e48c8ce998ae8b9b81dbe88691
ttolofari <tolofarit@gmail.com>
2024-07-04 17:38:13 -0700

    capitalized

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,6 +4,7 @@
  * string_toupper - a function that changes all lowercase letters of a string to uppercase.
  * @str : input string.
  *
+ * Return : output string
  */
 
 char *string_toupper(char *str)
