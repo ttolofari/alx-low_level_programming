e969fc3e7449a550a885859c7df04296e58284e7
ttolofari <tolofarit@gmail.com>
2024-07-17 14:12:32 -0700

    return name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main- a program that prints its
  * name, followed by a new line.
@@ -12,7 +11,6 @@
 int main(int argc, char *argv[])
 {
 	argc = argc;
-	
 	printf("%s\n", *argv);
 	return (0);
 }
