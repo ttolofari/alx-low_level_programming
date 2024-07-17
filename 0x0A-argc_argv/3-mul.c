e47a634712ffacff3e54ce3e8df809d3bb1af3af
ttolofari <tolofarit@gmail.com>
2024-07-17 15:51:34 -0700

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * _myatoi - converts a string to integer
- * @s: input string
+ * _myatoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: the resultant integer
+ * Return: the int converted from the string
  */
 int _myatoi(char *s)
 {
-	int result;
-	int q;
-	int p;
+	int i, d, n, len, f, digit;
 
-	result = 0;
-	q = 1;
-	p = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	while (s[p] == ' ')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		p++;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
 
-	if (s[p] == '-' ||  s[p] == '+')
-	{
-		q = (s[p++] == '-') ? -1 : 1;
-	}
+	if (f == 0)
+		return (0);
 
-	while (s[p] >= '0' && s[p] <= '9')
-	{
-		result = result * 10 + (s[p++] - '0');
-	}
-
-	return (result * q);
+	return (n);
 }
 
 /**
@@ -45,18 +55,19 @@ int _myatoi(char *s)
  */
 int main(int argc, char *argv[])
 {
+	int result, num1, num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int i = _myatoi(argv[1]);
-	int j = _myatoi(argv[2]);
-	int k = i * j;
+	num1 = _myatoi(argv[1]);
+	num2 = _myatoi(argv[2]);
+	result = num1 * num2;
 
-	printf("%d\n", k);
+	printf("%d\n", result);
 
 	return (0);
 }
-
