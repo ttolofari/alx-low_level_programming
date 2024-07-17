7210a22eb60aa89a22d863009227604996e29527
ttolofari <tolofarit@gmail.com>
2024-07-17 14:24:18 -0700

    return name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  * main - a program that prints its name
  * argc: number of arguments supplied
@@ -7,9 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
