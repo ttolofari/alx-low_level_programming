197d7fa180fc05a55850fb491254c79d19be01c5
ttolofari <tolofarit@gmail.com>
2024-07-17 14:15:37 -0700

    return name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	argc = argc;
 	printf("%s\n", *argv);
+
 	return (0);
 }
