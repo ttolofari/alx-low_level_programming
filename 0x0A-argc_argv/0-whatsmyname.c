922ad9a9a000d1de2768fe9dace8f80b1f22a8ef
ttolofari <tolofarit@gmail.com>
2024-07-17 14:10:33 -0700

    return name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - a program that prints its
+ * main- a program that prints its
  * name, followed by a new line.
  * argc: number of arguments supplied
  * argv: the array of arguments provided
@@ -12,6 +12,7 @@
 int main(int argc, char *argv[])
 {
 	argc = argc;
+	
 	printf("%s\n", *argv);
 	return (0);
 }
