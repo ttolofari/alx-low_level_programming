018eede920a78ad266df4eb06a2991d7663c81f8
ttolofari <tolofarit@gmail.com>
2024-09-19 18:02:30 -0700

    free node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked list.
+ * @head: The head of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
